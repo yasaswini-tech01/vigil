eed1422642e1f57b93255e79eac13d46b10132a1
yasaswini-tech01 <Yasaswini.Kothamasu@vithiit.com>
2026-01-23 12:26:44 +0530

    Refactor code structure for improved readability and maintainability

--- src/index.ts
+++ src/index.ts
@@ -1,133 +1,3 @@
-// import express from "express";
-// import http from "http";
-// import cors from "cors";
-// import bodyParser from "body-parser";
-// import { makeExecutableSchema } from "@graphql-tools/schema";
-// import { applyMiddleware } from "graphql-middleware";
-// import mercury from "@mercury-js/core";
-// import { ApolloServer } from "@apollo/server";
-// import { expressMiddleware } from "@apollo/server/express4";
-// import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-// import * as dotenv from "dotenv";
-// // import { RedisCache } from "@mercury-js/plugins/redis";
-// import "./models";
-// import "./profiles";
-// import "./hooks";
-// import { initSocket } from "./socket/socket";
-
-// import { google } from "googleapis";
-// import open from "open";
-// import { typeDefs, resolvers } from "./elastic-search";
-// import { setContext } from "./helpers/setContext";
-
-// // 2. Initialize the SDK
-
-// // If you want to parse the JSON back into a TS object:
-
-// dotenv.config();
-// // mercury.plugins([
-// //   new RedisCache({
-// //     client: { url: process.env.REDIS_URL, socket: { tls: false } },
-// //   }),
-// //   new historyTracking.HistoryTracking({ skipModels: ["Action"] }),
-// // ]);
-// export const app = express();
-// app.use(bodyParser.json({ limit: "200mb" }));
-// app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
-// app.use(express.json({ limit: "200mb" }));
-// app.use(express.urlencoded({ extended: true, limit: "200mb" }));
-// const corsOptions = {
-//   origin: "*",
-//   credentials: true,
-//   optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
-// const schema = applyMiddleware(
-//   makeExecutableSchema({
-//     typeDefs: [
-//       mercury.typeDefs,
-//       typeDefs,         
-//     ],
-//     resolvers: [
-//       mercury.resolvers,
-//       resolvers,    
-//     ],
-//   })
-// );
-
-// const DB_URL = process.env.DB_URL!;
-// mercury.connect(DB_URL);
-// (async function startApolloServer() {
-//   try {
-//     const httpServer = http.createServer(app);
-//     initSocket(httpServer);
-//     const server = new ApolloServer({
-//       introspection: true,
-//       schema,
-//       plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-//       rootValue: () => ({
-//         mercuryResolvers: mercury.resolvers,
-//       }),
-//     });
-//     await server.start();
-//     app.use(
-//       "/graphql",
-//       cors<cors.CorsRequest>(corsOptions),
-//       expressMiddleware(server, {
-//         context: async ({ req }) => await setContext(req),
-//       })
-//     );
-//     const PORT = process.env.PORT || 4005;
-//     await new Promise<void>((resolve) =>
-//       httpServer.listen({ port: PORT }, resolve)
-//     );
-//     console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
-
-// // Start server
-// app.listen(3000, () => {
-//   console.log("Server running at http://localhost:3000");
-//   console.log("Visit /auth/google to start login");
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   } catch (error) {
-//     console.error("❌ Error starting server:", error);
-//     process.exit(1);
-//   }
-// })();
-
 import express from "express";
 import http from "http";
 import cors from "cors";
@@ -139,17 +9,20 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import * as dotenv from "dotenv";
-
 import "./models";
 import "./profiles";
 import "./hooks";
-<<<<<<< HEAD
 import "./utils";
 import { google } from "googleapis";
 import { typeDefs, resolvers } from "./elastic-search";
 import { setContext } from "./helpers/setContext";
 import dotenv from "dotenv";
 import { emailQueue, messageWorker } from "./utils/queue";
+const app = express();
+app.use(bodyParser.json({ limit: "200mb" }));
+app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ extended: true, limit: "200mb" }));
 
 // ✅ Ensure worker is listening when server starts
 console.log("📢 Initializing message worker...");
@@ -157,47 +30,25 @@ if (messageWorker) {
   console.log("✅ Worker instance is ready");
 }
 
-// 2. Initialize the SDK
-// If you want to parse the JSON back into a TS object:
 
-dotenv.config();
-// mercury.plugins([
-//   new RedisCache({
-//     client: { url: process.env.REDIS_URL, socket: { tls: false } },
-//   }),
-//   new historyTracking.HistoryTracking({ skipModels: ["Action"] }),
-// ]);
 export const app = express();
-=======
 
 import { initSocket } from "./socket/socket";
-import { typeDefs, resolvers } from "./elastic-search";
-import { setContext } from "./helpers/setContext";
-//import { sendMessage } from "./utils/message";
+
 
 dotenv.config();
->>>>>>> 52aedeb4a2df2168a7e04e586b7cac31c91f5bf3
 
-/* -------------------- EXPRESS APP -------------------- */
 
-const app = express();
 
-app.use(bodyParser.json({ limit: "200mb" }));
-app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
-app.use(express.json({ limit: "200mb" }));
-app.use(express.urlencoded({ extended: true, limit: "200mb" }));
 
-<<<<<<< HEAD
 // Schema Setup
 mercury.addGraphqlSchema(typeDefs, resolvers);
-=======
 const corsOptions = {
   origin: "*",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
->>>>>>> 52aedeb4a2df2168a7e04e586b7cac31c91f5bf3
 const schema = applyMiddleware(
   makeExecutableSchema({
     typeDefs,
@@ -205,17 +56,11 @@ const schema = applyMiddleware(
   })
 );
 
-<<<<<<< HEAD
 const DB_URL = process.env.DB_URL || process.env.MONGO_URL; 
 if (!DB_URL) {
   console.error("❌ DB_URL is missing in .env file");
   process.exit(1);
 }
-=======
-/* -------------------- DB -------------------- */
-
-const DB_URL = process.env.DB_URL!;
->>>>>>> 52aedeb4a2df2168a7e04e586b7cac31c91f5bf3
 mercury.connect(DB_URL);
 
 /* -------------------- SERVER BOOTSTRAP -------------------- */
@@ -244,7 +89,6 @@ mercury.connect(DB_URL);
         context: async ({ req }) => await setContext(req),
       })
     );
-<<<<<<< HEAD
     app.get("/auth/google/callback", async (req, res) => {
   try {
     console.log("hiii");
@@ -363,24 +207,6 @@ mercury.connect(DB_URL);
     await new Promise<void>((resolve) => httpServer.listen({ port: PORT }, resolve));
     console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
     console.log(`🔗 Auth ready at http://localhost:${PORT}/auth/google`);
-=======
-    // app.get("/test-message", (req, res) => {
-    //   sendMessage("user_1", "user_123", "Hello from User 1 👋");
-    //   res.send("Message sent");
-    // });
-
-    // 🔥 START ONLY THIS SERVER
-    const PORT = process.env.PORT || 3000;
-
-    await new Promise<void>((resolve) =>
-      httpServer.listen(PORT, resolve)
-    );
-
-    //console.log(`🚀 Server running at http://localhost:${PORT}`);
-     console.log(`🚀 GraphQL ready at http://localhost:${PORT}/graphql`);
-    // console.log("🚀 Socket.IO ready on same port");
-
->>>>>>> 52aedeb4a2df2168a7e04e586b7cac31c91f5bf3
   } catch (error) {
     console.error("❌ Error starting server:", error);
     process.exit(1);
